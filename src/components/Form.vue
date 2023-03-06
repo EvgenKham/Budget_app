23f4a58b5f8e622fc4fb4e0d63c2c2cac5261875
EvgenKh <hamitcevich@gmail.com>
2023-03-06 17:57:49 +0300

    First commit

---
+++ src/components/Form.vue
@@ -0,0 +1,47 @@
+<template>
+  <el-card class="form-card">
+    <el-form :model="formData">
+      <el-form-item label="Type" prop="type">
+        <el-select class="type-select" v-model="formData.type" placeholder="Choose type...">
+          <el-option label="Income" value="INCOME"/>
+          <el-option label="Outcome" value="OUTCOME"/>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="Comments" prop="comment">
+        <el-input v-model="formData.comment" />
+      </el-form-item>
+      <el-form-item label="Value" prop="value">
+        <el-input v-model="formData.value" />
+      </el-form-item>
+      <el-button @click="onSubmit" type="primary">Submit</el-button>
+    </el-form>
+  </el-card>
+</template>
+
+<script>
+export default {
+  name: 'Form',
+  data: () => ({
+    formData: {
+      type: 'INCOME',
+      comment: '',
+      value: 0,
+    },
+  }),
+  methods: {
+    onSubmit() {
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+.form-card {
+  max-width: 500px;
+  margin: auto;
+}
+.type-select {
+  width: 100%;
+}
+</style>
