a0234b47ee1da16f434af03ee628a8eee9499162
EvgenKh <hamitcevich@gmail.com>
2023-03-08 17:25:23 +0300

    finish first task

--- src/components/BudgetList.vue
+++ src/components/BudgetList.vue
@@ -2,10 +2,8 @@
   <div class="budget-list-wrap">
     <el-card :header="header">
       <template v-if="!isEmpty">
-        <div class="list-item" v-for="(item, prop) in list" :key="prop">
-          <span class="budget-comment">{{ item.comment }}</span>
-          <span class="budget-value">{{ item.value }}</span>
-          <el-button type="danger" size="mini" @click="deleteItem(item.id)">Delete</el-button>
+        <div v-for="(item, prop) in list" :key="prop">
+          <BudgetListItem :item="item" @deleteItem="deleteItem"></BudgetListItem>
         </div>
       </template>
       <el-alert v-else type="info" :title="emptyTitle" :closable="false"/>
@@ -14,8 +12,13 @@
 </template>
 
 <script>
+import BudgetListItem from '@/components/BudgetListItem';
+
 export default {
   name: "BudgetList",
+  components: {
+    BudgetListItem
+  },
   props: {
     list: {
       type: Object,
@@ -43,15 +46,5 @@ export default {
 .budget-list-wrap {
   max-width: 500px;
   margin: auto;
-}
-.list-item {
-  display: flex;
-  align-items: center;
-  padding: 10px 15px;
-}
-.budget-value {
-  font-weight: bold;
-  margin-left: auto;
-  margin-right: 20px;
 }
 </style>
---
+++ src/components/BudgetListItem.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="budget-list-item">
+    <span class="budget-comment">{{ item.comment }}</span>
+    <span class="budget-value">{{ item.value }}</span>
+    <el-button type="danger" size="mini" @click="deleteItem(item.id)">Delete</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BudgetListItem",
+  data: () => ({
+    one: 'sdsd',
+  }),
+  props: {
+    item: {
+      value: 0,
+      comment: '',
+      id: 0,
+    }
+  },
+  methods: {
+    deleteItem(id) {
+      this.$emit('deleteItem', id);
+    }
+  }
+}
+</script>
+
+<style scoped>
+.budget-list-item {
+  display: flex;
+  align-items: center;
+  padding: 10px 15px;
+}
+.budget-value {
+  font-weight: bold;
+  margin-left: auto;
+  margin-right: 20px;
+}
+</style>
